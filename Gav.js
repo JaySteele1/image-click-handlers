28f9c43186131fce7df6d00a53350b34ef175327
Jay Steele <36495066+JaySteele1@users.noreply.github.com>
2018-08-19 12:29:25 -0600

    Create Gav.js

---
+++ Gav.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+
+    // Start of the click handlers for each picture. All in order 1-17 including all the cat Id's
+    $('#pic1').click(function() {
+        alert("some text")
+    });
+    $('#pic2').click(function() {
+        alert("some text")
+    });
+    $('#pic3').click(function() {
+        alert("some text")
+    });
+    $('#pic4').click(function() {
+        alert("some text")
+    });
+    $('#pic5').click(function() {
+        alert("some text")
+    });
+    $('#pic6').click(function() {
+        alert("some text")
+    });
+    $('#pic7').click(function() {
+        alert("some text")
+    });
+    $('#pic8').click(function() {
+        alert("some text")
+    });
+    $('#pic9').click(function() {
+        alert("some text")
+    });
+    $('#pic10').click(function() {
+        alert("some text")
+    });
+    $('#pic11').click(function() {
+        alert("some text")
+    });
+    $('#pic12').click(function() {
+        alert("some text")
+    });
+    $('#cat1').click(function() {
+        alert("some text")
+    });
+    $('#cat1-5').click(function() {
+        alert("some text")
+    });
+    $('#cat2').click(function() {
+        alert("some text")
+    });
+    $('#cat3').click(function() {
+        alert("some text")
+    });
+    $('#cat4').click(function() {
+        alert("some text")
+    });
+});
+
+
+/************************** VANILLA ***************** JS **************************/
+/*
+document.getElementById("pic1").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic2").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic3").addEventListener("click", function() {
+        alert("some text")    
+});
+document.getElementById("pic4").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic5").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic6").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic7").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic8").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic9").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic10").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic11").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("pic12").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("cat1").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("cat1-5").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("cat2").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("cat3").addEventListener("click", function() {
+        alert("some text")
+});
+document.getElementById("cat4").addEventListener("click", function() {
+        alert("some text")
+});
+*/
